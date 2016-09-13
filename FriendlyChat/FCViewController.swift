267e602c7fe70e80710710610d87eeaeab890c39
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2016-09-13 22:19:37 +0100

    Firebase iOS Codelab - Step 9 - Send Image Messages

--- FriendlyChat/FCViewController.swift
+++ FriendlyChat/FCViewController.swift
@@ -180,17 +180,35 @@ class FCViewController: UIViewController, UITableViewDataSource, UITableViewDele
 
     // if it's a photo from the library, not an image from the camera
     if let referenceUrl = info[UIImagePickerControllerReferenceURL] {
-      let assets = PHAsset.fetchAssetsWithALAssetURLs([referenceUrl as! NSURL], options: nil)
-      let asset = assets.firstObject
-      asset?.requestContentEditingInputWithOptions(nil, completionHandler: { (contentEditingInput, info) in
-        let imageFile = contentEditingInput?.fullSizeImageURL
-        let filePath = "\(FIRAuth.auth()?.currentUser?.uid)/\(Int(NSDate.timeIntervalSinceReferenceDate() * 1000))/\(referenceUrl.lastPathComponent!)"
-      })
+        let assets = PHAsset.fetchAssetsWithALAssetURLs([referenceUrl as! NSURL], options: nil)
+        let asset = assets.firstObject
+        asset?.requestContentEditingInputWithOptions(nil, completionHandler: { (contentEditingInput, info) in
+            let imageFile = contentEditingInput?.fullSizeImageURL
+            let filePath = "\(FIRAuth.auth()?.currentUser?.uid)/\(Int(NSDate.timeIntervalSinceReferenceDate() * 1000))/\(referenceUrl.lastPathComponent!)"
+            self.storageRef.child(filePath)
+                .putFile(imageFile!, metadata: nil) { (metadata, error) in
+                    if let error = error {
+                        print("Error uploading: \(error.description)")
+                        return
+                    }
+                    self.sendMessage([Constants.MessageFields.imageUrl: self.storageRef.child((metadata?.path)!).description])
+            }
+        })
     } else {
-      let image = info[UIImagePickerControllerOriginalImage] as! UIImage
-      let imageData = UIImageJPEGRepresentation(image, 0.8)
-      let imagePath = FIRAuth.auth()!.currentUser!.uid +
-        "/\(Int(NSDate.timeIntervalSinceReferenceDate() * 1000)).jpg"
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let imageData = UIImageJPEGRepresentation(image, 0.8)
+        let imagePath = FIRAuth.auth()!.currentUser!.uid +
+            "/\(Int(NSDate.timeIntervalSinceReferenceDate() * 1000)).jpg"
+        let metadata = FIRStorageMetadata()
+        metadata.contentType = "image/jpeg"
+        self.storageRef.child(imagePath)
+            .putData(imageData!, metadata: metadata) { (metadata, error) in
+                if let error = error {
+                    print("Error uploading: \(error)")
+                    return
+                }
+                self.sendMessage([Constants.MessageFields.imageUrl: self.storageRef.child((metadata?.path)!).description])
+        }
     }
   }
 
